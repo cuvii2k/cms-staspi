b1d7fe0809ad42887cceae4c905c12d306c4911c
DESKTOP-7CL0DD2\95vin <95vinhndu1415@gmail.com>
2022-12-05 14:40:32 +0700

    installed,aws-s3 upload provider

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,18 @@
-'use strict';
-
+"use strict";
 /**
  * post controller
  */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  getCategory: async (ctx) => {
+    try {
+      const entry = await strapi.db.query("api::post.post").findMany({
+        select: ["id", "title", "type"],
+      });
+      ctx.body = { data: entry };
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
---
+++ src/api/post/routes/get-categories.js
@@ -0,0 +1,9 @@
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/posts/category",
+      handler: "post.getCategory",
+    },
+  ],
+};
